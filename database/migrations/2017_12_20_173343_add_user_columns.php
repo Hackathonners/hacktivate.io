6a9254fabe8b261bcb5af1b08ca59a6e4227181f
jhugobb <hugo.b.brandao@gmail.com>
2017-12-20 17:39:18 +0000

    Add foreign keys migrations

---
+++ database/migrations/2017_12_20_173343_add_user_columns.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('users', function (Blueprint $table) {
+          $table->integer('team_id')->nullable()->unsigned();
+          $table->integer('owner_team_id')->nullable()->unsigned();
+          $table->integer('role_id')->nullable()->unsigned();
+
+          $table->foreign('team_id')->references('id')->on('team');
+          $table->foreign('owner_team_id')->references('owner_id')->on('team');
+          $table->foreign('role_id')->references('id')->on('role');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
