4bfee1c381978e6df54c73d354494eff09f2213c
jhugobb <hugo.b.brandao@gmail.com>
2017-12-20 18:09:45 +0000

    Fix lint issues

--- database/migrations/2017_12_20_173343_add_user_columns.php
+++ database/migrations/2017_12_20_173343_add_user_columns.php
@@ -13,15 +13,15 @@ class AddUserColumns extends Migration
      */
     public function up()
     {
-      Schema::table('users', function (Blueprint $table) {
-          $table->integer('team_id')->nullable()->unsigned();
-          $table->integer('owner_team_id')->nullable()->unsigned();
-          $table->integer('role_id')->nullable()->unsigned();
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('team_id')->nullable()->unsigned();
+            $table->integer('owner_team_id')->nullable()->unsigned();
+            $table->integer('role_id')->nullable()->unsigned();
 
-          $table->foreign('team_id')->references('id')->on('team');
-          $table->foreign('owner_team_id')->references('owner_id')->on('team');
-          $table->foreign('role_id')->references('id')->on('role');
-      });
+            $table->foreign('team_id')->references('id')->on('team');
+            $table->foreign('owner_team_id')->references('owner_id')->on('team');
+            $table->foreign('role_id')->references('id')->on('role');
+        });
     }
 
     /**
