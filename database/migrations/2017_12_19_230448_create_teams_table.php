8b1f8382cd50faabc902ca83c4b4ea2e450770f9
Tânia Silva <tdosilva@gmail.com>
2017-12-20 18:34:17 +0000

    Linter fix

--- database/migrations/2017_12_19_230448_create_teams_table.php
+++ database/migrations/2017_12_19_230448_create_teams_table.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Database\Migrations\Migration;
 
 class CreateTeamsTable extends Migration
 {
