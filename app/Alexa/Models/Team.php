bf316d00e65429070ba19da614d32d53e37f5a97
Tânia Silva <tdosilva@gmail.com>
2017-12-20 16:51:05 +0000

    Add teams model and migration

---
+++ app/Alexa/Models/Team.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Alexa\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+  /**
+   * The relations to eager load on every query.
+   *
+   * @var array
+   */
+   protected $with = ['team_name','project_name'];
+
+   /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+   protected $fillable = ['bio'];
+
+   /**
+    * Get user who owns this team.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+   public function owner() {
+     return $this->belongsTo(User::class);
+   }
+
+   /**
+    * Get user who owns this team.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+   public function users() {
+     return $this->hasMany(User::class);
+   }
+}
---
+++ database/migrations/2017_12_19_230448_create_teams_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('owner_id')->unisigned();
+            $table->string('team_name')->unique();
+            $table->string('project_name')->unique()->nullable();
+            $table->text('bio')->nullable();
+            $table->timestamps();
+
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
