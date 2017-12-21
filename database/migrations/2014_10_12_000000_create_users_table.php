dd2be87a26fb32b2c4b748e795d69e5c6bde79c2
jhugobb <hugo.b.brandao@gmail.com>
2017-12-21 16:22:25 +0000

    Resolve discussions

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->string('github');
             $table->string('phone_number')->unsigned()->nullable();
             $table->enum('gender', ['F', 'M', 'Other']);
@@ -26,8 +28,6 @@ class CreateUsersTable extends Migration
             $table->string('study_level')->nullable();
             $table->text('special_needs')->nullable();
             $table->text('bio')->nullable();
-            $table->string('email')->unique();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2017_12_20_173343_add_foreign_columns_to_users_table.php
+++ database/migrations/2017_12_20_173343_add_foreign_columns_to_users_table.php
@@ -14,6 +14,7 @@ class AddForeignColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+<<<<<<< 425191c7f11a92e52a1aff35c4a1d5137df11f2b
             $table->integer('team_id')->nullable()->unsigned();
             $table->integer('owner_team_id')->nullable()->unsigned();
             $table->integer('role_id')->nullable()->unsigned();
@@ -21,6 +22,13 @@ class AddForeignColumnsToUsersTable extends Migration
             $table->foreign('team_id')->references('id')->on('team');
             $table->foreign('owner_team_id')->references('owner_id')->on('team');
             $table->foreign('role_id')->references('id')->on('role');
+=======
+            $table->integer('team_id')->unsigned()->nullable();
+            $table->integer('role_id')->unsigned();
+
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->foreign('role_id')->references('id')->on('roles');
+>>>>>>> Resolve discussions
         });
     }
 
