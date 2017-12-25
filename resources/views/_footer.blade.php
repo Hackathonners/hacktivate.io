3c285568094be5ed188ef18a5f160aa3148f83ad
Francisco Neves <hi@francisconeves.me>
2017-12-25 23:37:53 +0000

    Tweak views

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -3,18 +3,10 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-4 text-center text-sm-right d-block d-sm-none">
 				<p class="text-muted small">
-					<a href="mailto:hello@hacktivate.io">
-						<i class="fas fa-lg fa-envelope pr-2"></i>
-					</a>
-					<a href="//twitter.com/hackathonners">
-						<i class="fab fa-lg fa-twitter pr-2"></i>
-					</a>
-					<a href="//github.com/hackathonners">
-						<i class="fab fa-lg fa-github pr-2"></i>
-					</a>
-					<a href="//hacktivate-hackathon.slack.com">
-						<i class="fab fa-lg fa-slack-hash"></i>
-					</a>
+					<a href="mailto:hello@hacktivate.io" class="mr-2"><i class="fas fa-lg fa-envelope"></i></a>
+					<a href="//twitter.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-twitter"></i></a>
+					<a href="//github.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-github"></i></a>
+					<a href="//hacktivate-hackathon.slack.com"><i class="fab fa-lg fa-slack-hash"></i></a>
 				</p>
 			</div>
 			<div class="col-xs-12 col-sm-8 text-center text-sm-left">
@@ -28,15 +20,9 @@
 			</div>
 			<div class="col-xs-12 col-sm-4 text-center text-sm-right d-none d-sm-block">
 				<p class="mb-1">
-					<a href="//twitter.com/hackathonners">
-						<i class="fab fa-lg fa-twitter pr-2"></i>
-					</a>
-					<a href="//github.com/hackathonners">
-						<i class="fab fa-lg fa-github pr-2"></i>
-					</a>
-					<a href="//hacktivate-hackathon.slack.com">
-						<i class="fab fa-lg fa-slack-hash"></i>
-					</a>
+					<a href="//twitter.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-twitter"></i></a>
+					<a href="//github.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-github"></i></a>
+					<a href="//hacktivate-hackathon.slack.com"><i class="fab fa-lg fa-slack-hash"></i></a>
 				</p>
 				<a class="small" href="mailto:hello@hacktivate.io">hello@hacktivate.io</a>
 			</div>
