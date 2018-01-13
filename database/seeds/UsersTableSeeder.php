5e72afd19450c3cbd7ac1a3616886bd31adcd48c
Diogo Couto <diogo2couto@gmail.com>
2018-01-13 00:47:18 +0000

    Fix

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -25,12 +25,12 @@ class UsersTableSeeder extends Seeder
         $adminRole = Role::whereType(Role::ROLE_ADMINISTRATOR)->first();
         foreach ($admins as $user) {
             User::create([
-                'name' => $user['name'],
-                'email' => $user['email'],
-                'github' => $user['github'],
-                'password' => bcrypt(Str::random()),
-                'role_id' => $adminRole->id,
-          ]);
+                        'name' => $user['name'],
+                        'email' => $user['email'],
+                        'github' => $user['github'],
+                        'password' => bcrypt(Str::random()),
+                        'role_id' => $adminRole->id,
+            ]);
         }
     }
 }
