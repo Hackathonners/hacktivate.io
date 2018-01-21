77c6f95cb8c14f3c8d7f2548dbe8a901e5a2373f
Francisco Neves <hi@francisconeves.me>
2018-01-21 23:28:43 +0000

    Add Google Analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,6 +40,17 @@
 
 	<!-- Styles -->
 	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <!-- Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112836254-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-112836254-1');
+    </script>
+
 </head>
 <body class="@yield('body-class')">
 	@yield('body')
