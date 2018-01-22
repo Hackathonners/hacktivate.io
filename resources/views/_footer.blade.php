4e55c600271cc4d907fd75e67e58f8fb2cdd9381
Francisco Neves <hi@francisconeves.me>
2018-01-22 00:04:19 +0000

    Fix footer year and indentation

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,33 +1,35 @@
 <div class="py-4 section-footer">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-4 text-center text-sm-right d-block d-sm-none">
-				<p class="text-muted small">
-					<a href="mailto:hello@hacktivate.io" class="mr-2"><i class="fas fa-lg fa-envelope"></i></a>
-					<a href="//twitter.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-twitter"></i></a>
-					<a href="//facebook.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-facebook-f"></i></a>
-					<a href="//github.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-github"></i></a>
-					<a href="//medium.com/Hackathonners" class="mr-2"><i class="fab fa-lg fa-medium"></i></a>
-				</p>
-			</div>
-			<div class="col-xs-12 col-sm-8 text-center text-sm-left">
-				<p class="mb-1">Built for you with
-					<i class="fas fa-heart text-primary"></i> &
-					<i class="fas fa-coffee"></i> by
-					<a href="http://hackathonners.org">Hackathonners</a>
-				</p>
-				<span class="text-muted small">&copy; Copyright 2017
-					<a class="text-muted" href="//hackathonners.org" target="_blank">Hackathonners</a>. All rights reserved.</span>
-			</div>
-			<div class="col-xs-12 col-sm-4 text-center text-sm-right d-none d-sm-block">
-				<p class="mb-1">
-					<a href="//twitter.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-twitter"></i></a>
-					<a href="//facebook.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-facebook-f"></i></a>
-					<a href="//github.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-github"></i></a>
-					<a href="//medium.com/Hackathonners" class="mr-2"><i class="fab fa-lg fa-medium"></i></a>
-				</p>
-				<a class="small" href="mailto:hello@hacktivate.io">hello@hacktivate.io</a>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 text-center text-sm-right d-block d-sm-none">
+                <p class="text-muted small">
+                    <a href="mailto:hello@hacktivate.io" class="mr-2"><i class="fas fa-lg fa-envelope"></i></a>
+                    <a href="//twitter.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-twitter"></i></a>
+                    <a href="//facebook.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-facebook-f"></i></a>
+                    <a href="//github.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-github"></i></a>
+                    <a href="//medium.com/Hackathonners" class="mr-2"><i class="fab fa-lg fa-medium"></i></a>
+                </p>
+            </div>
+            <div class="col-xs-12 col-sm-8 text-center text-sm-left">
+                <p class="mb-1">Built for you with
+                    <i class="fas fa-heart text-primary"></i> &
+                    <i class="fas fa-coffee"></i> by
+                    <a href="http://hackathonners.org">Hackathonners</a>
+                </p>
+                <span class="text-muted small">&copy; Copyright {{ date('Y') }}
+                    <a class="text-muted" href="//hackathonners.org" target="_blank">Hackathonners</a>. All rights reserved.</span>
+                }
+                }
+            </div>
+            <div class="col-xs-12 col-sm-4 text-center text-sm-right d-none d-sm-block">
+                <p class="mb-1">
+                    <a href="//twitter.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-twitter"></i></a>
+                    <a href="//facebook.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-facebook-f"></i></a>
+                    <a href="//github.com/hackathonners" class="mr-2"><i class="fab fa-lg fa-github"></i></a>
+                    <a href="//medium.com/Hackathonners" class="mr-2"><i class="fab fa-lg fa-medium"></i></a>
+                </p>
+                <a class="small" href="mailto:hello@hacktivate.io">hello@hacktivate.io</a>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-	<link rel="manifest" href="/favicon/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 
-	<meta name="description" content="Apply now to the Hacktivate hackathon, organized by Hackathonners. It is a programming marathon for students to create something outstanding within 24 hours.">
+    <meta name="description" content="Apply now to the Hacktivate hackathon, organized by Hackathonners. It is a programming marathon for students to create something outstanding within 24 hours.">
     <meta name="keywords" content="hackathon, hackathonners, event, fun, programming, marathon, organizer, development, ideas, fun, team, building, group, continuous, learning">
     <meta name="author" content="Hackathonners">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,12 +34,12 @@
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:creator" content="@Hackathonners" />
 
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>Hacktivate — February 10-11 2018, Universidade do Minho</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Hacktivate — February 10-11 2018, Universidade do Minho</title>
 
-	<!-- Styles -->
-	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
     <!-- Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112836254-1"></script>
@@ -53,11 +53,11 @@
 
 </head>
 <body class="@yield('body-class')">
-	@yield('body')
+    @yield('body')
 
     @include('_footer')
 
-	<script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     @stack('scripts')
 </body>
 </html>
